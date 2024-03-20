9233bf78eb12858dc8dc080e80a39dd1cb144530
M Panczesnik <m.panczesnik@pjwstk.edu.pl>
2024-03-20 10:14:55 +0100

    init

---
+++ untitled/src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    String name;
+    String surname;
+    String s;
+
+    Student(String name, String surname, String s){
+        this.name = name;
+        this.surname = surname;
+        this.s = s;
+    }
+}
