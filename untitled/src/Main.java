fd17844ec1de21b37d2e084caf690f9152dab95e
M Panczesnik <m.panczesnik@pjwstk.edu.pl>
2024-03-20 10:26:39 +0100

    update

---
+++ untitled/src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        Student Student1 = new Student("Andrzej", "Koluszko", "s11111");
+        System.out.printf(Student1.toString() + "\n");
+        //StudentNo - your index number
+        Student s10101 = new Student("YourName", "YourSurname", "YourStudentsNumber");
+        System.out.printf(s10101.toString()+ "\n");
+    }
+}
--- untitled/src/Student.java
+++ untitled/src/Student.java
@@ -8,4 +8,8 @@ public class Student {
         this.surname = surname;
         this.s = s;
     }
+
+    public String toString() {
+        return this.name + " " + this.surname + " " + this.s;
+    }
 }
