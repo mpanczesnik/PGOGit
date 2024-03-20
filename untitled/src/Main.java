018b0eb6dadd37b448bb70e7410bb23e50405e1f
MARCEL GROSZKOWSKI <s30689@pjwstk.edu.pl>
2024-03-20 10:51:50 +0100

    commit

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        Student Student1 = new Student("Andrzej", "Koluszko", "s11111");
+        Student Student1 = new Student("Marcel", "Groszkowski", "s30689
         System.out.printf(Student1.toString() + "\n");
         //StudentNo - your index number
-        Student s10101 = new Student("YourName", "YourSurname", "YourStudentsNumber");
-        System.out.printf(s10101.toString()+ "\n");
+        Student s30689 = new Student("YourName", "YourSurname", "YourStudentsNumber");
+        System.out.printf(s30689.toString()+ "\n");
     }
 }
